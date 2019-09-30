e10faafdc5232b12975e00a977fa2bcc9be63843
Junior <junior.jaava@gmail.com>
2019-09-30 21:25:44 +0200

    Working with throw exceptions and verify times(0)

--- teste-de-unidade/src/br/com/caelum/leilao/servico/EncerradorDeLeilao.java
+++ teste-de-unidade/src/br/com/caelum/leilao/servico/EncerradorDeLeilao.java
@@ -5,27 +5,37 @@ import java.util.List;
 
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.infra.dao.LeilaoDao;
+import br.com.caelum.leilao.infra.email.Carteiro;
 
 public class EncerradorDeLeilao {
 
-    private int encerrados;
-    private final LeilaoDao dao;
+	private int encerrados;
+	private final LeilaoDao dao;
+	private final Carteiro carteiro;
 
-    public EncerradorDeLeilao(LeilaoDao dao) {
-        this.dao = dao;
-    }
+	public EncerradorDeLeilao(LeilaoDao dao, Carteiro carteiro) {
+		this.dao = dao;
+		// guardamos o carteiro como atributo da classe
+		this.carteiro = carteiro;
+	}
 
-    public void encerra() {
-        List<Leilao> todosLeiloesCorrentes = dao.correntes();
+	public void encerra() {
+		List<Leilao> todosLeiloesCorrentes = dao.correntes();
 
-        for(Leilao leilao : todosLeiloesCorrentes) {
-            if(comecouSemanaPassada(leilao)) {
-                encerrados++;
-                leilao.encerra();
-                dao.salva(leilao);
-            }
-        }
-    }
+		for (Leilao leilao : todosLeiloesCorrentes) {
+			try {
+				if (comecouSemanaPassada(leilao)) {
+					encerrados++;
+					leilao.encerra();
+					dao.salva(leilao);
+					carteiro.envia(leilao);
+				}
+			} catch (Exception e) {
+				// salvo a excecao no sistema de logs
+				// e o loop continua!
+			}
+		}
+	}
 
 	private boolean comecouSemanaPassada(Leilao leilao) {
 		return diasEntre(leilao.getData(), Calendar.getInstance()) >= 7;
--- teste-de-unidade/test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
+++ teste-de-unidade/test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -1,5 +1,13 @@
 package br.com.caelum.leilao.servico;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
@@ -9,10 +17,7 @@ import org.junit.Test;
 import br.com.caelum.leilao.builder.CriadorDeLeilao;
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.infra.dao.LeilaoDao;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import br.com.caelum.leilao.infra.email.Carteiro;
 
 public class EncerradorDeLeilaoTest {
 
@@ -29,30 +34,54 @@ public class EncerradorDeLeilaoTest {
 		LeilaoDao daoFalso = mock(LeilaoDao.class);
 		when(daoFalso.correntes()).thenReturn(leiloesAntigos);
 
-		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso);
+		Carteiro carteiroFalso = mock(Carteiro.class);
+
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso, carteiroFalso);
 		encerrador.encerra();
 
 		assertTrue(leilao1.isEncerrado());
 		assertTrue(leilao2.isEncerrado());
 		assertEquals(2, encerrador.getQuantidadeDeEncerrados());
 	}
-	
-	
-    @Test
-    public void deveAtualizarLeiloesEncerrados() {
 
-        Calendar antiga = Calendar.getInstance();
-        antiga.set(1999, 1, 20);
+	@Test
+	public void deveAtualizarLeiloesEncerrados() {
 
-        Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma")
-            .naData(antiga).constroi();
+		Calendar antiga = Calendar.getInstance();
+		antiga.set(1999, 1, 20);
 
-        LeilaoDao daoFalso = mock(LeilaoDao.class);
-        when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1));
+		Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma").naData(antiga).constroi();
 
-        EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso);
-        encerrador.encerra();
+		LeilaoDao daoFalso = mock(LeilaoDao.class);
+		when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1));
 
-        verify(daoFalso, times(1)).salva(leilao1);
-    }
+		Carteiro carteiroFalso = mock(Carteiro.class);
+
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso, carteiroFalso);
+		encerrador.encerra();
+
+		verify(daoFalso, times(1)).salva(leilao1);
+	}
+
+	@Test
+	public void deveContinuarAExecucaoMesmoQuandoDaoFalha() {
+		Calendar antiga = Calendar.getInstance();
+		antiga.set(1999, 1, 20);
+
+		Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma").naData(antiga).constroi();
+		Leilao leilao2 = new CriadorDeLeilao().para("Geladeira").naData(antiga).constroi();
+
+		LeilaoDao daoFalso = mock(LeilaoDao.class);
+		when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1, leilao2));
+
+		doThrow(new RuntimeException()).when(daoFalso).salva(leilao1);
+
+		Carteiro carteiroFalso = mock(Carteiro.class);
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso, carteiroFalso);
+
+		encerrador.encerra();
+
+		verify(daoFalso, times(0)).atualiza(leilao1);
+		verify(carteiroFalso, times(0)).envia(leilao1);
+	}
 }
