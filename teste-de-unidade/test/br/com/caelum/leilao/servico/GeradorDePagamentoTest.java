300830dda4fe57ac6fcde07cbab460476f45edfc
Junior <junior.jaava@gmail.com>
2019-10-02 20:11:39 +0200

    Capturing received args by Mock. Using ArgumentCaptor

--- teste-de-unidade/test/br/com/caelum/leilao/servico/GeradorDePagamentoTest.java
+++ teste-de-unidade/test/br/com/caelum/leilao/servico/GeradorDePagamentoTest.java
@@ -1,14 +1,11 @@
 package br.com.caelum.leilao.servico;
 
-import java.util.Arrays;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
 
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
