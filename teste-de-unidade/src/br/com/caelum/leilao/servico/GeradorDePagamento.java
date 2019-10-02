3ea6b628710d7d06f9975b5bd0613c44ad7b89c9
Junior <junior.jaava@gmail.com>
2019-10-02 20:40:22 +0200

    Isolate to test. Isolate a static instance(Calendar.getInstance)  in an interface Relogio to help to mock

--- teste-de-unidade/src/br/com/caelum/leilao/servico/GeradorDePagamento.java
+++ teste-de-unidade/src/br/com/caelum/leilao/servico/GeradorDePagamento.java
@@ -7,30 +7,51 @@ import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Pagamento;
 import br.com.caelum.leilao.infra.dao.RepositorioDeLeiloes;
 import br.com.caelum.leilao.infra.dao.RepositorioDePagamentos;
+import br.com.caelum.leilao.infra.relogio.Relogio;
+import br.com.caelum.leilao.infra.relogio.RelogioDoSistema;
 
 public class GeradorDePagamento {
 
-    private final RepositorioDePagamentos pagamentos;
-    private final RepositorioDeLeiloes leiloes;
-    private final Avaliador avaliador;
+	private final RepositorioDePagamentos pagamentos;
+	private final RepositorioDeLeiloes leiloes;
+	private final Avaliador avaliador;
+	private final Relogio relogio;
 
-    public GeradorDePagamento(RepositorioDeLeiloes leiloes, 
-            RepositorioDePagamentos pagamentos, 
-            Avaliador avaliador) {
-        this.leiloes = leiloes;
-        this.pagamentos = pagamentos;
-        this.avaliador = avaliador;
-    }
+	public GeradorDePagamento(RepositorioDeLeiloes leiloes, RepositorioDePagamentos pagamentos, Avaliador avaliador) {
+		this.leiloes = leiloes;
+		this.pagamentos = pagamentos;
+		this.avaliador = avaliador;
+		this.relogio = new RelogioDoSistema();
+	}
 
-    public void gera() {
+	public GeradorDePagamento(RepositorioDeLeiloes leiloes, RepositorioDePagamentos pagamentos, Avaliador avaliador,
+			Relogio relogio) {
+		this.leiloes = leiloes;
+		this.pagamentos = pagamentos;
+		this.avaliador = avaliador;
+		this.relogio = relogio;
+	}
 
-        List<Leilao> leiloesEncerrados = leiloes.encerrados();
-        for(Leilao leilao : leiloesEncerrados) {
-            avaliador.avalia(leilao);
+	public void gera() {
 
-            Pagamento novoPagamento = 
-                    new Pagamento(avaliador.getMaiorLance(), Calendar.getInstance());
-            pagamentos.salva(novoPagamento);
-        }
-    }
+		List<Leilao> leiloesEncerrados = leiloes.encerrados();
+		for (Leilao leilao : leiloesEncerrados) {
+			avaliador.avalia(leilao);
+
+			Pagamento novoPagamento = new Pagamento(avaliador.getMaiorLance(), primeiroDiaUtil());
+			pagamentos.salva(novoPagamento);
+		}
+	}
+
+	private Calendar primeiroDiaUtil() {
+		Calendar data = relogio.hoje();
+		int diaDaSemana = data.get(Calendar.DAY_OF_WEEK);
+
+		if (diaDaSemana == Calendar.SATURDAY)
+			data.add(Calendar.DAY_OF_MONTH, 2);
+		else if (diaDaSemana == Calendar.SUNDAY)
+			data.add(Calendar.DAY_OF_MONTH, 1);
+
+		return data;
+	}
 }
